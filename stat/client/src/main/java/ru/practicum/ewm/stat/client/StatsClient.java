98f8b0b9665e9d83c50d2da7cb422406f304299b
Michail <m.ananev2014@mail.ru>
2024-02-17 15:09:36 +0300

    2

--- stat/client/src/main/java/ru/practicum/ewm/stat/client/StatsClient.java
+++ stat/client/src/main/java/ru/practicum/ewm/stat/client/StatsClient.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.stat.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +10,10 @@ import ru.practicum.ewm.stat.dto.HitGetDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +24,12 @@ public class StatsClient {
     private final RestTemplate restTemplate;
 
 
-    public void create(HitCreateDto hitCreateDto) throws HttpStatusCodeException{
+    public void create(HitCreateDto hitCreateDto) throws HttpStatusCodeException {
         restTemplate.postForLocation(serverUrl.concat("/hit"), hitCreateDto);
     }
 
     public List<HitGetDto> get(LocalDateTime start, LocalDateTime end,
-                               List<String> uris, boolean unique) throws HttpStatusCodeException{
+                               List<String> uris, boolean unique) throws HttpStatusCodeException {
         Map<String, Object> parameters = new HashMap<>(Map.of(
                 "start", start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
                 "end", end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
@@ -36,10 +38,10 @@ public class StatsClient {
         if (uris != null && !uris.isEmpty()) {
             parameters.put("uris", String.join(",", uris));
         }
-            HitGetDto[] response = restTemplate.getForObject(
-                    serverUrl.concat("/stats?start={start}&end={end}&uris={uris}&unique={unique}"),
-                    HitGetDto[].class, parameters);
-        
+        HitGetDto[] response = restTemplate.getForObject(
+                serverUrl.concat("/stats?start={start}&end={end}&uris={uris}&unique={unique}"),
+                HitGetDto[].class, parameters);
+
         return Objects.isNull(response)
                 ? List.of()
                 : List.of(response);
